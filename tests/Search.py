c34d249941c19ddf9f46cdcbe0f1802cd68befef
shashi <sksharan666@gmail.com>
2023-12-30 14:33:38 +0530

    few tests added

--- tests/Search.py
+++ tests/Search.py
@@ -24,3 +24,35 @@ def test_search_for_valid_products():
     assert ele.is_displayed()
 
     driver.quit()
+
+
+def test_search_for_an_invalid_product():
+    driver=webdriver.Chrome()
+    driver.maximize_window()
+    driver.get("https://tutorialsninja.com/demo/")
+    time.sleep(2)
+    driver.find_element(By.NAME, "search").send_keys("HONDA")
+    time.sleep(2)
+    driver.find_element(By.XPATH, "//button[contains(@class,'btn-default btn-lg')]").click()
+    time.sleep(2)
+    print("NEXT ASSERTION line printed here")
+    ele_text= driver.find_element(By.XPATH,"//input[@id='button-search']/following-sibling::p").text
+    expected_text="There is no product that matches the search criteria."
+    assert ele_text.__eq__(expected_text)
+    driver.quit()
+
+
+def test_search_without_entering_any_product():
+    driver=webdriver.Chrome()
+    driver.maximize_window()
+    driver.get("https://tutorialsninja.com/demo/")
+    time.sleep(2)
+    driver.find_element(By.NAME, "search").send_keys("")
+    time.sleep(2)
+    driver.find_element(By.XPATH, "//button[contains(@class,'btn-default btn-lg')]").click()
+    time.sleep(2)
+    print("NEXT ASSERTION line printed here")
+    ele_text= driver.find_element(By.XPATH,"//input[@id='button-search']/following-sibling::p").text
+    expected_text="There is no product that matches the search criteria."
+    assert ele_text.__eq__(expected_text)
+    driver.quit()
